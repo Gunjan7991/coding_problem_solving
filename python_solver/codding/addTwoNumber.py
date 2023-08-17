7916d4ee814dcf5c1a491c08fa236df982540223
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-17 16:41:25 -0500

    add two number from leetcode

--- python_solver/codding/addTwoNumber.py
+++ python_solver/codding/addTwoNumber.py
@@ -1,10 +1,21 @@
 from typing import Optional
-from linkedList import ListNode
+from .mylist import SingleListNode
 
-def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    while l1.next == None:
-        print(l1.val)
-        l1 = l1.next
-    print(l1.val)
-    
 
+def addTwoNumbers(l1: Optional[SingleListNode], l2: Optional[SingleListNode]) -> Optional[SingleListNode]:
+    v1: str = ""
+    v2: str = ""
+    while l1 != None or l2 != None:
+        if l1 != None:
+            v1 += str(l1.val)
+            l1 = l1.next
+
+        if l2 != None:
+            v2 += str(l2.val)
+            l2 = l2.next
+    v1, v2 = v1[::-1], v2[::-1]
+    value = int(v1) + int(v2)
+    prev = None
+    for i in str(value):
+        prev = SingleListNode(int(i), prev)
+    return prev
