88756e805f2d3c7ba147c3974ea945a1251eeb8d
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-17 16:41:44 -0500

    name changed

---
+++ python_solver/codding/mylist.py
@@ -0,0 +1,16 @@
+class SingleListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def list_convert(lst: list[any]) -> "SingleListNode":
+        head = None
+        for val in lst:
+            head = SingleListNode(val, next=head)
+        return head
+    def list_print(linkedList: "SingleListNode") -> None:
+        while linkedList != None:
+            if linkedList.next != None:
+                print(linkedList.val, end="->")
+            print(linkedList.val)
+            linkedList = linkedList.next
