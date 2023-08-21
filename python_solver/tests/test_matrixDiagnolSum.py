41a64a766fcb9a0025c943ced05c76d82baec3ff
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-20 19:17:56 -0500

    formatting

--- python_solver/tests/test_matrixDiagnolSum.py
+++ python_solver/tests/test_matrixDiagnolSum.py
@@ -1,16 +1,35 @@
 from codding.MatrixDiagnolSum import diagonalSum
 
+
 def test_diagonalSum_1():
-    assert diagonalSum([[1,2,3],[4,5,6],[7,8,9]]) == 25
+    assert diagonalSum([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 25
+
 
 def test_diagonalSum_2():
-    assert diagonalSum([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]) == 8
+    assert diagonalSum([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]) == 8
+
 
 def test_diagonalSum_3():
-    assert diagonalSum([[5],]) == 5
+    assert (
+        diagonalSum(
+            [
+                [5],
+            ]
+        )
+        == 5
+    )
+
 
 def test_diagonalSum_4():
-    assert diagonalSum([[0],]) == 0
+    assert (
+        diagonalSum(
+            [
+                [0],
+            ]
+        )
+        == 0
+    )
+
 
 def test_diagonalSum_5():
-    assert diagonalSum([[1,2],[3,4]]) == 10
+    assert diagonalSum([[1, 2], [3, 4]]) == 10
