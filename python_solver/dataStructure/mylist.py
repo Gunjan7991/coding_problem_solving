390bea83a7238f0e4c74f7db8c8a4e2edbca0ded
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-25 16:35:29 -0500

    insertion in both stack and queue order

--- python_solver/dataStructure/mylist.py
+++ python_solver/dataStructure/mylist.py
@@ -1,17 +1,35 @@
+from  typing import Optional
 class SingleListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
     def list_convert(lst: list[any]) -> "SingleListNode":
+        """
+        Appends the values in the fron and converts the given list to LinkedList in reverse order.
+        """
         head = None
         for val in lst:
             head = SingleListNode(val, next=head)
         return head
+    
+    def list_convert2(lst: list[any]) -> Optional["SingleListNode"]:
+        """
+        Appends the values in the back and converts the given list to LinkedList as it is.
+        """
+        head = SingleListNode()
+        temp = head
+        for val in lst:
+            temp.next = SingleListNode(val)
+            temp = temp.next
+        
+        return head.next
 
     def list_print(linkedList: "SingleListNode") -> None:
-        while linkedList != None:
-            if linkedList.next != None:
+        while linkedList:
+            if not linkedList:
                 print(linkedList.val, end="->")
-            print(linkedList.val)
+            else:
+                print(linkedList.val)
             linkedList = linkedList.next
+        return None
