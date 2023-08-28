66c69ed20a605d09c720405706d68a6773afc316
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-27 19:19:46 -0500

    re-factored

---
+++ python_solver/tests/leetcode/test_medianSortedArrays.py
@@ -0,0 +1,44 @@
+from leetcode.medianOfTwoSortedArray import (
+    findMedianSortedArrays,
+    findMedianSortedArraysFast,
+)
+
+
+def test_findMedianSortedArrays_1():
+    assert findMedianSortedArrays([1, 3], [2]) == 2.00000
+
+
+def test_findMedianSortedArrays_2():
+    assert findMedianSortedArrays([1, 2], [3, 4]) == 2.50000
+
+
+def test_findMedianSortedArrays_3():
+    assert findMedianSortedArrays([1, 2, 3], [4, 5, 6]) == 3.50000
+
+
+def test_findMedianSortedArrays_4():
+    assert findMedianSortedArrays([5, 6, 7], [9]) == 6.50000
+
+
+def test_findMedianSortedArrays_5():
+    assert findMedianSortedArrays([1], [1]) == 1.00000
+
+
+def test_findMedianSortedArraysFast_1():
+    assert findMedianSortedArraysFast([1, 3], [2]) == 2.00000
+
+
+def test_findMedianSortedArraysFast_2():
+    assert findMedianSortedArraysFast([1, 2], [3, 4]) == 2.50000
+
+
+def test_findMedianSortedArraysFast_3():
+    assert findMedianSortedArraysFast([1, 2, 3], [4, 5, 6]) == 3.50000
+
+
+def test_findMedianSortedArraysFast_4():
+    assert findMedianSortedArraysFast([5, 6, 7], [9]) == 6.50000
+
+
+def test_findMedianSortedArraysFast_5():
+    assert findMedianSortedArraysFast([1], [1]) == 1.00000
