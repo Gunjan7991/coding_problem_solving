1e94d45e6a8713ed79a298d734d2f6f6e5f89eb2
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-27 19:19:30 -0500

    re-factored

---
+++ python_solver/tests/leetcode/test_twoSumProblem.py
@@ -0,0 +1,21 @@
+from leetcode.twoSumProblem import twoSum
+
+
+def test_twoSum_1():
+    assert twoSum([2, 7, 11, 15], 9) == [0, 1]
+
+
+def test_twoSum_2():
+    assert twoSum([3, 2, 4], 6) == [1, 2]
+
+
+def test_twoSum_3():
+    assert twoSum([3, 3], 6) == [0, 1]
+
+
+def test_twoSum_4():
+    assert twoSum([1, 3, 5, 7], 6) == [0, 2]
+
+
+def test_twoSum_5():
+    assert twoSum([11, 12], 40) == []
